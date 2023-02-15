d676b28cbe6f066c2924fd769b032f1e901bb6eb
naliwka <avierina.nataliia@gmail.com>
2023-02-16 01:42:31 +0200

    Update Program.cs in Lab_1-1

--- Lab_1-1/Program.cs
+++ Lab_1-1/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_1_1
 {
     internal class Program
-    {
+    {  
         static void Main(string[] args)
         {
+            Console.Write("Enter the numbers:\na = ");
+            double a  = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the arithmetic mark - ");
+            char mark = Convert.ToChar(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+            switch (mark)
+            {
+                case '+':
+                    {
+                        Console.WriteLine(Add(a, b));
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine(Sub(a, b));
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine(Mul(a, b));
+                        break;
+                    }
+                case '/':
+                    {
+                        if 
+                            (b != 0) Console.WriteLine(Div(a, b)); 
+                        else 
+                            Console.WriteLine("It’s impossible to divide by zero.");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Entered incorrect mark.");
+                        break;
+                    }
+            }
+            double Add(double x, double y) => x + y;
+            double Sub(double x, double y) => x - y;
+            double Mul(double x, double y) => x * y;
+            double Div(double x, double y) => x / y;
         }
     }
 }
