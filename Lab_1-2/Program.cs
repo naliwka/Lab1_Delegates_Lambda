e2829e77d334d879b9d70e4b7d6c4a26879377e8
naliwka <avierina.nataliia@gmail.com>
2023-02-23 15:01:54 +0200

    Update Program.cs in Lab_1-2

--- Lab_1-2/Program.cs
+++ Lab_1-2/Program.cs
@@ -10,6 +10,29 @@ namespace Lab_1_2
     {
         static void Main(string[] args)
         {
+            // Create an array with three delegates that return a random value of type int
+            Random random = new Random();
+
+            Func<int>[] methods = {
+                () => random.Next(100),
+                () => random.Next(100),
+                () => random.Next(100)
+            };
+
+            // Create an anonymous method that accepts an array of delegates and returns the average arithmetic value
+            Func<Func<int>[], double> average = delegate (Func<int>[] delegates)
+            {
+                int sum = 0;
+                foreach (var method in delegates)
+                {
+                    sum += method();
+                }
+                return (double)sum / delegates.Length;
+            };
+
+            // Call an anonymous method with an array of delegates
+            double result = average(methods);
+            Console.WriteLine($"Average arithmetic value: {result}");
         }
     }
 }
