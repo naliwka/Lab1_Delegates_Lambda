bffcf555f257f0e7e06e2dd4cdda318b98c16600
naliwka <avierina.nataliia@gmail.com>
2023-02-23 15:08:28 +0200

    Update Program.cs in Lab_1-3

--- Lab_1-3/Program.cs
+++ Lab_1-3/Program.cs
@@ -10,6 +10,16 @@ namespace Lab_1_3
     {
         static void Main(string[] args)
         {
+            // Сreate an anonymous method that accepts three integer arguments and returns their average arithmetic value
+            Func<int, int, int, double> average = delegate (int a, int b, int c)
+            {
+                return (double)(a + b + c) / 3;
+            };
+
+            // Call the method
+            double result = average(100, 68, 14);
+
+            Console.WriteLine($"Середнє арифметичне значення: {result}");
         }
     }
 }
